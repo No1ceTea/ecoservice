10d458528c51aada87289d023581edfc145849c8
No1ceTea <thaiwomtg@gmail.com>
2022-12-20 00:47:19 +0100

    Seeding

---
+++ pages/api/seed.js
@@ -0,0 +1,13 @@
+import data from '../../utils/data'
+import db from '../../utils/db'
+import User from '../../models/User'
+
+const handler = async (req, res) => {
+  await db.connect()
+  await User.deleteMany()
+  await User.insertMany(data.users)
+  await db.disconnect()
+  res.send({ message: 'seeded succesfully'})
+}
+
+export default handler
--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+  users: [
+    {
+      name: 'John',
+      email: 'john@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'jane@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: 'Free Shirt',
