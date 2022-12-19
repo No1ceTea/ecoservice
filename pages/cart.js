d7bce6ce189d7b6820225e28d876e6d55d638785
No1ceTea <thaiwomtg@gmail.com>
2022-12-19 07:14:15 +0100

    add login

--- pages/cart.js
+++ pages/cart.js
@@ -88,7 +88,7 @@ function cartScreen() {
                   </div>
                 </li>
                 <li>
-                  <button onClick={() => router.push('/shipping')} className="primary-button w-full">
+                  <button onClick={() => router.push('login?redirect=/shipping')} className="primary-button w-full">
                     Checkout
                   </button>
                 </li>
