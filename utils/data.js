7b97dffd93d01572970116964342b988170cb400
No1ceTea <thaiwomtg@gmail.com>
2022-12-19 01:27:27 +0100

    dev

---
+++ utils/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt',
+    },
+    {
+      name: 'Fit-Shirt',
+      slug: 'fit-shirt',
+      category: 'Shirts',
+      image: '/images/shirt2.jpg',
+      price: 80,
+      brand: 'Adidas',
+      rating: 3.2,
+      numReviews: 10,
+      countInStock: 10,
+      description: 'A popular shirt',
+    },
+    {
+      name: 'Fit Pants',
+      slug: 'fit-pants',
+      category: 'Pants',
+      image: '/images/pants1.jpg',
+      price: 95,
+      brand: 'Zara',
+      rating: 3.5,
+      numReviews: 7,
+      countInStock: 20,
+      description: 'A popular pants',
+    },
+  ],
+};
+
+export default data;
