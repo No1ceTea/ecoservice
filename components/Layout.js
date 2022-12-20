a818114de9d243161276e62f1c284929c0477302
No1ceTea <thaiwomtg@gmail.com>
2022-12-20 14:15:00 +0100

    login with mongoose

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { useContext, useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react'
+
 import { Store } from '../utils/Store'
 
 export default function Layout({title, children}) {
 
-  const { state, dispatch } = useContext(Store)
+  const { status, data: session } = useSession()
+
+  const { state } = useContext(Store)
   const { cart } = state
   const [cartItemsCount, setCartItemsCount] = useState(0)
   useEffect(() => {
@@ -21,6 +27,8 @@ export default function Layout({title, children}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <ToastContainer position='bottom-center' limit={1} />
+
       <div className='flex min-h-screen flex-col justify-between'>
         <header>
           <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
@@ -28,14 +36,27 @@ export default function Layout({title, children}) {
               <a className='text-lg font-bold'>Amazona</a>
             </Link>
             <div>
-              <Link href="/cart" legacyBehavior><a className='p-2'>Cart
-              {cartItemsCount > 0 && (
-                <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
-                  {cartItemsCount}
-                </span>
+              <Link href="/cart" legacyBehavior>
+                <a className='p-2'>
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
+              
+              {status === 'loading' ?(
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login" legacyBehavior>
+                  <a className="p-2 ">Login</a>
+                </Link>
               )}
-              </a></Link>
-              <Link href="/login" legacyBehavior><a className='p-2'>Login</a></Link>
+            
             </div>
           </nav>
         </header>
--- models/User.js
+++ models/User.js
@@ -4,13 +4,13 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true},
-    isAdmin: { type: Boolean, required: true, default: false},
-  } , {
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
     timestamps: true,
   }
-)
+);
 
-const User = mongoose.model.User || mongoose.model('User', userSchema)
-
+const User = mongoose.models.User || mongoose.model('User', userSchema)
 export default User
