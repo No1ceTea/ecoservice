42b7fe422455b5fe420c45e271edb0ce7a569fd8
No1ceTea <thaiwomtg@gmail.com>
2023-07-10 22:54:20 +0200

    Traduction part.6

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -65,41 +65,41 @@ export default function PlaceOrderScreen() {
   return (
     <Layout title="Place Order">
       <CheckoutWizard activeStep={3} />
-      <h1 className="mb-4 text-xl">Place Order</h1>
+      <h1 className="mb-4 text-xl">Récapitulatif de la commande</h1>
       {cartItems.length === 0 ? (
         <div>
-          Cart is empty. <Link href="/">Go shopping</Link>
+          Le panier est vide. <Link href="/">Aller le remplir </Link>
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
           <div className="overflow-x-auto md:col-span-3">
             <div className="card  p-5">
-              <h2 className="mb-2 text-lg">Shipping Address</h2>
+              <h2 className="mb-2 text-lg">Adresse de livraison</h2>
               <div>
                 {shippingAddress.fullName}, {shippingAddress.address},{' '}
                 {shippingAddress.city}, {shippingAddress.postalCode},{' '}
                 {shippingAddress.country}
               </div>
               <div>
-                <Link href="/shipping">Edit</Link>
+                <Link href="/shipping">Modifier</Link>
               </div>
             </div>
             <div className="card  p-5">
-              <h2 className="mb-2 text-lg">Payment Method</h2>
+              <h2 className="mb-2 text-lg">Méthode de paiement</h2>
               <div>{paymentMethod}</div>
               <div>
-                <Link href="/payment">Edit</Link>
+                <Link href="/payment">Modifier</Link>
               </div>
             </div>
             <div className="card overflow-x-auto p-5">
-              <h2 className="mb-2 text-lg">Order Items</h2>
+              <h2 className="mb-2 text-lg">Article</h2>
               <table className="min-w-full">
                 <thead className="border-b">
                   <tr>
                     <th className="px-5 text-left">Item</th>
-                    <th className="    p-5 text-right">Quantity</th>
-                    <th className="  p-5 text-right">Price</th>
-                    <th className="p-5 text-right">Subtotal</th>
+                    <th className="p-5 text-right">Quantité</th>
+                    <th className="p-5 text-right">Prix</th>
+                    <th className="p-5 text-right">Total</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -121,46 +121,46 @@ export default function PlaceOrderScreen() {
 
                         </Link>
                       </td>
-                      <td className=" p-5 text-right">{item.quantity}</td>
-                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">{item.price}€</td>
                       <td className="p-5 text-right">
-                        ${item.quantity * item.price}
+                        {item.quantity * item.price}€
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
               <div>
-                <Link href="/cart">Edit</Link>
+                <Link href="/cart">Modifier</Link>
               </div>
             </div>
           </div>
           <div>
             <div className="card  p-5">
-              <h2 className="mb-2 text-lg">Order Summary</h2>
+              <h2 className="mb-2 text-lg">Total</h2>
               <ul>
                 <li>
                   <div className="mb-2 flex justify-between">
                     <div>Items</div>
-                    <div>${itemsPrice}</div>
+                    <div>{itemsPrice} €</div>
                   </div>
                 </li>
                 <li>
                   <div className="mb-2 flex justify-between">
-                    <div>Tax</div>
-                    <div>${taxPrice}</div>
+                    <div>Taxe</div>
+                    <div>{taxPrice} €</div>
                   </div>
                 </li>
                 <li>
                   <div className="mb-2 flex justify-between">
-                    <div>Shipping</div>
-                    <div>${shippingPrice}</div>
+                    <div>Frais de livraison</div>
+                    <div>{shippingPrice} €</div>
                   </div>
                 </li>
                 <li>
                   <div className="mb-2 flex justify-between">
                     <div>Total</div>
-                    <div>${totalPrice}</div>
+                    <div>{totalPrice} €</div>
                   </div>
                 </li>
                 <li>
@@ -169,7 +169,7 @@ export default function PlaceOrderScreen() {
                     onClick={placeOrderHandler}
                     className="primary-button w-full"
                   >
-                    {loading ? 'Loading...' : 'Place Order'}
+                    {loading ? 'Chargement...' : 'Commander'}
                   </button>
                 </li>
               </ul>
