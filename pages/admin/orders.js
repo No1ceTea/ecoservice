bd67f21aabdcc2ab28fa37e1f6ee5b6f2a80bd21
No1ceTea <thaiwomtg@gmail.com>
2023-07-11 11:44:30 +0200

    fix dashboard

--- pages/admin/orders.js
+++ pages/admin/orders.js
@@ -87,7 +87,7 @@ export default function AdminOrderScreen() {
                       <td className="p-5">
                         {order.createdAt?.substring(0, 10)}
                       </td>
-                      <td className="p-5">${order.totalPrice}</td>
+                      <td className="p-5">{order.totalPrice}€</td>
                       <td className="p-5">
                         {order.isPaid
                           ? `${order.paidAt?.substring(0, 10)}`
